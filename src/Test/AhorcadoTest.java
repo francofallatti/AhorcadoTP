c507e1413efc844fe42c9bb62ef5a84e3b00b6c8
francofallatti <66625402+francofallatti@users.noreply.github.com>
2022-03-26 13:24:53 -0300

    first commit

---
+++ src/Test/AhorcadoTest.java
@@ -0,0 +1,19 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AhorcadoTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
